76d0db0f8c8f5ee849635d6e359777e4a7965681
Vemund Santi <vemund@santi.no>
2017-05-11 23:55:30 +0200

    Use sieve of erasthenes to find the sum of all circular prime numbers

---
+++ abakus-readme/spesielle-sykliske-siffer.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from math import sqrt, ceil
+"""
+Assignment: What is the sum of all cyclic prime numbers between 0 and 1000000?
+https://en.wikipedia.org/wiki/Circular_prime
+"""
+
+
+def generate_primes(end):
+    """Generate prime numbers
+    Uses the Sieve of Erasthenes algorithm to generate
+    all primes up to and including end
+    """
+    numbers = [_ for _ in range(2, end + 1)]
+
+    for i in range(ceil(sqrt(len(numbers)))):
+        if numbers[i]:
+            for multiple in range(2 * numbers[i], len(numbers) + 2, numbers[i]):
+                numbers[multiple - 2] = False
+    return [x for x in numbers if x]
+
+
+def cycle_digit(number):
+    return number[-1] + number[:-1]
+
+
+def find_cyclic_primes(numbers):
+    cyclic_numbers = []
+    numbers_dict = dict([(str(number), True) for number in numbers])
+    print(numbers_dict)
+    for number in numbers:
+        current_number = str(number)
+        cyclic = True
+        for _ in range(len(current_number)):
+            if not numbers_dict.get(current_number, False):
+                cyclic = False
+                break
+            current_number = cycle_digit(current_number)
+        if cyclic:
+            cyclic_numbers.append(number)
+    return cyclic_numbers
+
+
+print(sum(find_cyclic_primes(generate_primes(1000000))))
